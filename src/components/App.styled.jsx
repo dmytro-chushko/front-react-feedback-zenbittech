f88e7de6922c1742e9b7cc95fe6d66d4fc01a9ee
Dmytro Chushko <dmytro.chushko@gmail.com>
2022-10-10 01:05:20 +0300

    init

---
+++ src/components/App.styled.jsx
@@ -0,0 +1,10 @@
+import styled from 'styled-components';
+
+export const Title = styled.h1`
+  font-family: 'Apercu Arabic Pro';
+  font-style: normal;
+  font-weight: 400;
+  font-size: 40px;
+  line-height: 130%;
+  color: #3e3e3e;
+`;
